dbe93a3df096affd1cee2e9d1d5474a09aff4559
栗鑫 <lx389029980@outlook.com>
2018-01-25 22:21:31 +0800

    V1

--- forIteration/Program.cs
+++ forIteration/Program.cs
@@ -10,11 +10,28 @@ namespace forIteration
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++) // "++"为递增运算符
             {
-                Console.WriteLine(i);
+                /*
+                Console.WriteLine(i); // 功能1：显示每次迭代的i的值
+                */
+
+                if (i == 7) // 功能2：当迭代到7的时候，显示下列字符
+                {
+                    Console.WriteLine("Found seven!");
+                    break; //意思是跳出迭代，执行ReadLine
+                }
             }
+
+
+            for (int x = 0; x < 12; x++)
+            {
+                Console.Write(x);
+            }
+
+
             Console.ReadLine();
         }
     }
 }
+// 
